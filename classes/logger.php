8176f66aef3ddc7400156f14abc9e609de92f31b
benlacey57 <85163278+benlacey57@users.noreply.github.com>
2024-01-06 23:47:01 +0000

    Update logger.php

--- classes/logger.php
+++ classes/logger.php
@@ -1,34 +1,37 @@
 <?php
 
+enum LogLevel: string {
+    case INFO = 'INFO';
+    case ERROR = 'ERROR';
+    case DEBUG = 'DEBUG';
+}
+
+
 class Logger {
     private $logFilePath;
-
-    const INFO = 'INFO';
-    const ERROR = 'ERROR';
-    const DEBUG = 'DEBUG';
 
     public function __construct($logFilePath) {
         $this->logFilePath = $logFilePath;
     }
 
-    public function log($level, $message, $context = []) {
+    public function log(LogLevel $level, $message, $context = []) {
         $timestamp = date('Y-m-d H:i:s');
         $contextJson = !empty($context) ? json_encode($context) : '';
 
-        $logEntry = sprintf("[%s] %s: %s %s\n", $timestamp, $level, $message, $contextJson);
+        $logEntry = sprintf("[%s] %s: %s %s\n", $timestamp, $level->value, $message, $contextJson);
 
         file_put_contents($this->logFilePath, $logEntry, FILE_APPEND | LOCK_EX);
     }
 
     public function info($message, $context = []) {
-        $this->log(self::INFO, $message, $context);
+        $this->log(LogLevel::INFO, $message, $context);
     }
 
     public function error($message, $context = []) {
-        $this->log(self::ERROR, $message, $context);
+        $this->log(LogLevel::ERROR, $message, $context);
     }
 
     public function debug($message, $context = []) {
-        $this->log(self::DEBUG, $message, $context);
+        $this->log(LogLevel::DEBUG, $message, $context);
     }
 }
