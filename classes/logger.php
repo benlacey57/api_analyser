d40a8050824ac7cefd570f791de0a340015f8b3e
benlacey57 <85163278+benlacey57@users.noreply.github.com>
2024-01-06 23:59:45 +0000

    Update logger.php

--- classes/logger.php
+++ classes/logger.php
@@ -6,14 +6,23 @@ enum LogLevel: string {
     case DEBUG = 'DEBUG';
 }
 
-
 class Logger {
     private $logFilePath;
 
+    /**
+     * Constructor for the Logger class
+     * @param string $logFilePath Path to the log file
+     */
     public function __construct($logFilePath) {
         $this->logFilePath = $logFilePath;
     }
 
+    /**
+     * Logs a message with a specific level to the log file
+     * @param LogLevel $level The level of the log entry
+     * @param string $message The message to log
+     * @param array $context Additional context to log with the message
+     */
     public function log(LogLevel $level, $message, $context = []) {
         $logEntry = [
             'timestamp' => date('Y-m-d H:i:s'),
@@ -26,15 +35,34 @@ class Logger {
         file_put_contents($this->logFilePath, $jsonLogEntry, FILE_APPEND | LOCK_EX);
     }
 
+    /**
+     * Logs an informational message
+     * @param string $message The message to log
+     * @param array $context Additional context to log with the message
+     */
     public function info($message, $context = []) {
         $this->log(LogLevel::INFO, $message, $context);
     }
 
+    /**
+     * Logs an error message
+     * @param string $message The message to log
+     * @param array $context Additional context to log with the message
+     */
     public function error($message, $context = []) {
         $this->log(LogLevel::ERROR, $message, $context);
     }
 
+    /**
+     * Logs a debug message
+     * @param string $message The message to log
+     * @param array $context Additional context to log with the message
+     */
     public function debug($message, $context = []) {
         $this->log(LogLevel::DEBUG, $message, $context);
     }
 }
+
+// Usage
+// $logger = new Logger('/path/to/log.txt');
+// $logger->info("Informational message");
